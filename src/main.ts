b0e0621c81fbbac6a41f27ea3dfd884d68c7807b
ghazal tarek <A.Ahmed08579@student.aast.edu>
2026-04-24 20:20:37 +0300

    pet palnet

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose'
-import express from 'express'
-
+import mongoose from 'mongoose';
+import express from 'express';
 
 
 mongoose.connect('mongodb://localhost:27017/Pet').then(() => {
   console.log('Connected to MongoDB')
 }).catch((err) => {
   console.error('Failed to connect to MongoDB', err)
-})
+});
+
 
 const app=express()
 const port=3000
